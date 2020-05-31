30513ab03d1cf289164556b40afcbe84cf0dc491
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2020-05-31 19:51:04 +0700

    Update introduction week on 2020/05/31 19:50 WIB.

--- 01.Introduction/app/src/main/java/id/ariefpurnamamuharram/myapplication/MainActivity.kt
+++ 01.Introduction/app/src/main/java/id/ariefpurnamamuharram/myapplication/MainActivity.kt
@@ -1,19 +1,23 @@
 package id.ariefpurnamamuharram.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import id.ariefpurnamamuharram.myapplication.introduction.IntroductionActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imgHMBTN: ImageView
     private lateinit var btnStart: Button
 
-    // Main function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get button instance.
+        // Get components instance.
+        imgHMBTN = findViewById(R.id.img_hmbtn)
         btnStart = findViewById(R.id.btn_start)
 
         // Call buttonListener function.
@@ -22,9 +26,15 @@ class MainActivity : AppCompatActivity() {
 
     // Create buttonListener function.
     private fun buttonListener() {
+        // btnStart click listener.
         btnStart.setOnClickListener {
             val intent = Intent(this, IntroductionActivity::class.java)
             startActivity(intent)
+        }
+
+        // imgHMBTN click listener.
+        imgHMBTN.setOnClickListener {
+            Toast.makeText(this, getString(R.string.txt_hmbtn), Toast.LENGTH_SHORT).show()
         }
     }
 }
---
+++ 01.Introduction/app/src/main/java/id/ariefpurnamamuharram/myapplication/introduction/IntroductionActivity.kt
@@ -0,0 +1,12 @@
+package id.ariefpurnamamuharram.myapplication.introduction
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.ariefpurnamamuharram.myapplication.R
+
+class IntroductionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduction)
+    }
+}
